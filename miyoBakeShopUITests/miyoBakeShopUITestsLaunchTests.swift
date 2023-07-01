fbbd4db9cc21ca5ec044b1ee4de93a46d465bc3d
gokhuncelik <celik.gokhun@gmail.com>
2023-07-01 11:25:11 +0300

    Initial Commit

---
+++ miyoBakeShopUITests/miyoBakeShopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  miyoBakeShopUITestsLaunchTests.swift
+//  miyoBakeShopUITests
+//
+//  Created by Gokhun Celik on 1.07.2023.
+//
+
+import XCTest
+
+final class miyoBakeShopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
