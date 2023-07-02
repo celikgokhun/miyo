8d98990121340f10bb6f715534910813a4da0ada
gokhuncelik <celik.gokhun@gmail.com>
2023-07-02 15:56:03 +0300

    add custom view

---
+++ miyoBakeShop/FoodAndBeverageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FoodAndBeverageTableViewCell.swift
+//  miyoBakeShop
+//
+//  Created by Gokhun Celik on 2.07.2023.
+//
+
+import UIKit
+
+class FoodAndBeverageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
